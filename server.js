02153215b4379d1dce28708852880527473de021
CarlosCC07 <carlos.ccx10@gmail.com>
2019-02-14 13:42:12 -0600

    Added clima functionality

--- server.js
+++ server.js
@@ -23,6 +23,24 @@ app.post('/clima', function (req, res) {
   res.render('clima', {state, crumb});
 })
 
+app.post('/clima_upload', function (req, res) {
+  let escala = parseInt(req.body.escala_radio);
+  let empresa = parseInt(req.body.select_empresa);
+  let planta = req.body.planta_input;
+  let year = parseInt(req.body.year_input);
+  let generales = req.body.generales_input.toLowerCase();
+  let reactivos = req.body.reactivos_file;
+  let servicios_enabled = parseInt(req.body.servicios_check);
+  let comentarios_enabled = parseInt(req.body.comentarios_check);
+  let otras_enabled = parseInt(req.body.otras_check);
+  let servicios = req.body.servicios_input.toLowerCase();
+  let otras = parseInt(req.body.otras_input);
+  
+  let crumb = 'Nuevo Estudio';
+  state = 1;
+  res.render('clima', {state, crumb});
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
